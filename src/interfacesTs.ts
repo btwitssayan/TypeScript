1e51bc82b22a75fa48d38ea6625c52fe157d4d72
Sayan Golder <sayangolder2004@gmail.com>
2026-01-07 18:08:41 +0530

    completed interfaceTs file

--- src/interfacesTs.ts
+++ src/interfacesTs.ts
@@ -1,1 +1,61 @@
-function makeChaI(order:{})
+function makeChaI(order:{type:string, sugar:number,strong:boolean}){
+    console.log(order)
+}
+
+type TeaRecipe = {
+    water:number,
+    milk:number,
+    varitent:'normal' | 'strong'
+}
+
+class MasalaChai implements TeaRecipe{
+    water=100;
+    milk=50;
+    varitent:'normal' | 'strong' = 'normal';
+}
+
+interface CupSize {
+    size: 'small' | 'medium' | 'large';
+}
+
+class chai implements CupSize{
+    size: "small" | "medium" | "large" = 'large'
+}
+
+// type response = {ok:true} | {ok:false};
+
+// class myRes implements response{
+//     ok: true = true;
+// } 
+
+type TeaType = 'masala' | 'ginger' | 'lemon';
+
+function orderChai(t: TeaType){
+    console.log(t);
+}
+
+type BaseChai = {
+    teaLeaves: number
+}
+type Extra = {
+    masala:number
+}
+type MasalaChai2 = BaseChai & Extra;
+
+const mychai:MasalaChai2 = {
+    teaLeaves:10,
+    masala:5
+}
+
+type user ={
+    username:string,
+    bio?:string
+}
+const user1:user = {
+    username:'chai lover'
+}
+
+const user2:user = {
+    username:'tea lover',
+    bio:'I love tea'
+}
